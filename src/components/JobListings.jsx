9198a82a14c7c24f167c5a4e942a42ddf428a985
Robiul92 <robiullawal312@gmail.com>
2024-10-13 19:21:36 +0600

    Job listing page  updated

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Jobs from '../jobs.json'
 import JobListing from './JobListing';
 
-const JobListings = () => {
-   const recentListing = Jobs.slice(0,3);
+const JobListings = ({isHome = false}) => {
+   const allJobListing = isHome ? Jobs.slice(0,3) : Jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+         {isHome ? 'Browse Jobs' : 'Recent Jobs'} 
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           
           {
-            recentListing.map((job) => (
+            allJobListing.map((job) => (
 
                 <JobListing key = {job.id} job = {job} />
             )
--- src/pages/JobPages.jsx
+++ src/pages/JobPages.jsx
@@ -1,4 +1,3 @@
-
 import JobListings from '../components/JobListings'
 
 const JobPages = () => {
